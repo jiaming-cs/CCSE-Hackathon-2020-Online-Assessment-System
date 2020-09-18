1a843036a91fb99d255af41326c2ba3e3d5416f3
Jiaming <jiamingli9674@gmail.com>
2020-09-18 00:29:55 -0400

    push const.py for convenience

---
+++ const.py
@@ -0,0 +1,15 @@
+EMAIL_USER_NAME = "magmutualassessment@gmail.com"
+EMAIL_PASS_WORD = "test123456!"
+
+SCORE_MAP = {'Criteria Met':3,
+'Partial Compliance':2,
+'Criteria Not Met':1,
+'Always':4,
+'Almost Never':1,
+'Never':0,
+'Almost Always':3,
+'Sometimes':2,
+'NA':0,
+True:0
+}
+
